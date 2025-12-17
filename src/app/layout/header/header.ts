b246c98797d71f3b89e4a0ef8f89080b0724b8ba
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-12-17 20:18:52 +0530

    updated videos

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Header {
 
+  openWhatsApp() {
+  const phoneNumber = '916284500902'; // country code + number
+  const message = 'Hi, ViralStan! I want to know more about your services';
+
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  window.open(url, '_blank');
+}
 }
